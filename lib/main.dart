977385170d6d2fa08f60fb11193d5d1d717c794a
JCodez <jannjagi14@gmail.com>
2024-08-07 15:29:48 +0300

    Readme update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:fit_fusion/pages/details.dart';
 import 'package:fit_fusion/pages/onboarding.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Onboardingpage(),
     );
   }
--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -1,5 +1,4 @@
 import 'package:fit_fusion/pages/details.dart';
-import 'package:fit_fusion/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
